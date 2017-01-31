d05269f47b6c864b5c7f61ae34e58abf0c9003bb
gogobango <arthur.banckaert@gmail.com>
2017-01-31 12:05:36 +0100

    TP2

--- src/Blog/FrontBundle/Controller/DefaultController.php
+++ src/Blog/FrontBundle/Controller/DefaultController.php
@@ -8,6 +8,11 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BlogFrontBundle:Default:index.html.twig', array("title" => "Accueil Blog","content" => "Bienvenue sur notre blog"));
+        return $this->render('BlogFrontBundle:Default:index.html.twig', array("content" => "Bienvenue sur notre blog"));
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('BlogFrontBundle:Default:about.html.twig', array("content" => "A Propos : Ce blog présente des articles..."));
     }
 }
